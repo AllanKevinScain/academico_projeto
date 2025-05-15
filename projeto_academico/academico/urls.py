f125243c92934c6d216e6773929c9585e7ea0c9f
allankevinscain <meuemail44allan@gmail.com>
2025-05-14 21:57:09 -0300

    chore: mesma coisa com os cursos

--- projeto_academico/academico/urls.py
+++ projeto_academico/academico/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import index, alunos
+from .views import index, alunos, cursos
 from .views import cadastrar_aluno, cadastrar_curso
-from .views import editar_aluno
+from .views import editar_aluno, editar_curso
 
 urlpatterns = [
     path('', index, name='index'),
     path('alunos/', alunos, name='alunos'),
     path('cadastrar_aluno/', cadastrar_aluno, name='cadastrar_aluno'),
-    path('cadastrar_curso/', cadastrar_curso, name='cadastrar_curso'),
     path('editar_aluno/<int:id>/', editar_aluno, name='editar_aluno'),
+
+    path('cursos/', cursos, name='cursos'),
+    path('cadastrar_curso/', cadastrar_curso, name='cadastrar_curso'),
+    path('editar_curso/<int:id>/', editar_curso, name='editar_curso'),
 ]
--- projeto_academico/academico/views.py
+++ projeto_academico/academico/views.py
@@ -14,13 +14,6 @@ def alunos(request):
     return render(request, 'academico/aluno/lista_alunos.html', dados)
 
 
-# Nem ta sendo usado ainda
-def cursos(request):
-    cursos = Curso.objects.all()
-    dados = {'cursos': cursos}
-    return render(request, 'academico/aluno/lista_alunos.html', dados)
-
-
 def cadastrar_aluno(request):
     if request.method == 'POST':
         form = AlunoForm(request.POST)
@@ -31,24 +24,6 @@ def cadastrar_aluno(request):
         form = AlunoForm()
         dados = {'form': form}
     return render(request, 'academico/aluno/cadastrar_aluno.html', dados)
-
-
-def cadastrar_curso(request):
-    if request.method == 'POST':
-        form = CursoForm(request.POST)
-        if form.is_valid():
-            # is_valid() Vai validar os dados, o Tokken CSRF, e criar um
-            # dicionário com os dados chamado cleaned_data
-            # Aqui estamos pegando o nome do curso
-            curso = form.cleaned_data['nome']
-            print(curso)
-
-            form.save()
-            return redirect('index')
-    else:
-        form = CursoForm()
-        dados = {'form': form}
-    return render(request, 'academico/aluno/cadastrar_curso.html', dados)
 
 
 def editar_aluno(request, id):
@@ -67,3 +42,39 @@ def editar_aluno(request, id):
     dados = {'form': form, 'aluno': aluno}
 
     return render(request, 'academico/aluno/editar_aluno.html', dados)
+
+
+def cursos(request):
+    cursos = Curso.objects.all()
+    dados = {'cursos': cursos}
+    return render(request, 'academico/curso/lista_cursos.html', dados)
+
+
+def cadastrar_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = CursoForm()
+        dados = {'form': form}
+    return render(request, 'academico/curso/cadastrar_curso.html', dados)
+
+
+def editar_curso(request, id):
+    try:
+        curso = Curso.objects.get(id=id)
+    except:
+        return redirect('cursos')
+
+    if request.method == 'POST':
+        form = CursoForm(request.POST, instance=curso)
+        if form.is_valid():
+            form.save()
+            return redirect('cursos')
+
+    form = CursoForm(instance=curso)
+    dados = {'form': form, 'curso': curso}
+
+    return render(request, 'academico/curso/editar_curso.html', dados)
