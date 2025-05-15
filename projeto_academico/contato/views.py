3675d3ac95df83b78dc6991a7f4c4fc90e67edcf
allankevinscain <meuemail44allan@gmail.com>
2025-05-14 21:18:47 -0300

    chore: envio de email

--- projeto_academico/contato/views.py
+++ projeto_academico/contato/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from . import forms
+from django.core.mail import send_mail
 
 
 def contato(request):
-    form = forms.ContatoForm()
-    dados = {'form': form}
+    if request.method == 'POST':
+        form = forms.ContatoForm(request.POST)
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            corpo_email = f"Mensagem de: {nome}\n\n Email: {email}\n\n Mensagem: {mensagem}"
+
+            send_mail(
+                assunto,
+                corpo_email,
+                email,
+                ['meuemail44allan@gmail.com'],
+            )
+
+            form = forms.ContatoForm()
+            dados = {
+                'form': form,
+                'mensagem_enviada': True,
+                'mensagem': 'Mensagem enviada com sucesso!',
+            }
+
+    else:
+        form = forms.ContatoForm()
+        dados = {'form': form}
 
     return render(request, 'contato/contato.html', dados)
